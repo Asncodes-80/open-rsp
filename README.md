8164a826dd67b530898336922ec40e9c82fb4e29
asncodes-80 <asn80.asn@hotmail.com>
2023-04-05 15:09:58 +0330

    wip(format): add string formatter of snmpwalk result

--- README.md
+++ README.md
@@ -3,16 +3,14 @@
 A Rust microservice, connects to SNMP server, gets all SNMP data, parsing them
 and produces data to kafka broker.
 
+At `0.1.0` version, it get data from SNMP server through `snmpwalk` networking
+shell tool.
+
 ## RSP App Description
 
 ```sh
-./open-rsp --ip 5.201.128.78 --port 5051 --username publicMD5 --password publicMD5 --oid 1.3.6.1.6.3.15.1.13.44.0
+open-rsp -i 5.201.128.78 -p 5051 \
+ -u publicMD5 -k publicMD5 \ 
+ --oid "1.3.6.1.6.3.15.1.13.44.0" \ 
+ -t "my-topic"
 ```
-
-## References 
-
-Check this sites:
-
-- [Rust Snmp Simulator](https://sonalake.com/latest/an-open-source-rust-snmp-simulator/)
-- [Rust Snmp data collector](https://github.com/kporika/snmp-collection/blob/main/src/main.rs)
-- [Sexy Rust TUI](https://docs.rs/ratatui/latest/ratatui/)
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,3 @@
-/// Rust SNMP Producer
-///
-///      (None) `V1` TODO: Complete with real logical state
-///        | ip: String
-///        | port: String
-///        | topic: String
-///        v
-///   (UPD::connect())
-///       |\_____________
-///       |              | create::session()
-///       |              |
-///       |          [Produce]
-///    panic()           |\___________
-///                      |            |
-///                   panic()         | {CreateMessage()}
-///                                   | {Base-on Topic()}
-///                                   v
-///                       New-data-produced-to-Kafka
-///
-/// Logical State               __state
-/// ------------                ---------
-/// Idle                        _RSP_IDLE
-/// Udp-Session-created         _RSP_UDP_SES_CREATED
-/// Udp-Request-started         _RSP_UDP_STARTED
-/// Gather-Snmp-info            _RSP_DATA_COLLECT
-/// Parse-data                  _RSP_DATA_PARSE
-/// Produce-2Kafka              _RSP_PRODUCER
 use std::io;
 use std::process::{Command, Output};
 
@@ -48,7 +21,7 @@ fn rs_shell(args: &Args) -> Result<Output, io::Error> {
             "-a",
             &args.encryption,
             &format!("{}:{}", &args.ip, &args.port),
-            "1.3.6.1.6.3.15.1.13.44.0",
+            &args.oid,
             "-O",
             "q",
         ])
@@ -57,39 +30,45 @@ fn rs_shell(args: &Args) -> Result<Output, io::Error> {
     Ok(output)
 }
 
+/// Returned SNMPWalk value.
+#[derive(Debug, PartialEq)]
+enum SnmpValue<'a> {
+    Number(f32),
+    Str(&'a str),
+}
+
+impl<'a> SnmpValue<'a> {
+    pub fn new(value: &'a str) -> Self {
+        let evaled = value.trim_end_matches('\n').trim_matches('"');
+        let parsed = evaled.parse::<f32>();
+        match parsed {
+            Ok(v) => Self::Number(v),
+            Err(_) => Self::Str(evaled),
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
     simple_logger::init().unwrap();
-
-    let mut prev_value: f32 = 0.1;
+    // let mut prev_value: f32 = 0.1;
 
     let binding = rs_shell(&args).unwrap();
     let output = String::from_utf8_lossy(&binding.stdout).into_owned();
-    let snmp_output = output.split(" ").collect::<Vec<&str>>()[1].to_string();
-    log::info!("{}", snmp_output);
+    let last_value = {
+        let mut output_bindings = output.split(" ");
+        output_bindings.next();
+        let Some(output) = output_bindings
+            .next() else {
+                panic!("Invalid value received from the `snmpwalk` command.");
+            };
+        SnmpValue::new(output)
+    };
 
-    let mut producer: producer::Event = producer::Event::init(vec!["localhost:9092".to_string()]);
-    producer.send_data(&args.topic, String::from(format!("value: {}", snmp_output)));
-
-    // let str_output = output.split("=").collect::<Vec<&str>>()[1];
-    // println!("{}", str_output.to_owned());
-
-    // loop {
-    //     // ...
-    //     // Get snmp last data about an oid
-    //     // let socket = UdpSocket::bind(format!("{}:{}", args.ip, args.port)).await?;
-    //     // i.e. `next_value` parsed to a float value
-    //     let next_value: f32 = 0.0;
-
-    //     if prev_value != next_value {
-    //         prev_value = next_value;
+    log::info!("{:?}", last_value);
 
     // let mut producer: producer::Event = producer::Event::init(vec!["localhost:9092".to_string()]);
-    // producer.send_data(&args.topic, String::from(format!("value: {}", prev_value)));
-
-    //         log::info!("Value {} as new value, sent to to Kafka broker", prev_value);
-    //     }
-    // }
+    // producer.send_data(&args.topic, String::from(format!("value: {}", last_value)));
 }
 
 /// open-rsp - SNMP data gather and produce them to Kafka broker.
@@ -116,7 +95,23 @@ struct Args {
     #[arg(short, long, default_value_t = String::from("MD5"))]
     encryption: String,
 
+    /// OID: Object id of one sensor
+    #[arg(short, long)]
+    oid: String,
+
     /// Topic: Topic name to produce value to Kafka
     #[arg(short, long, default_value_t = String::from("my-topic"))]
     topic: String,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_snmp_value_new() {
+        assert_eq!(SnmpValue::new("salam"), SnmpValue::Str("salam"));
+        assert_eq!(SnmpValue::new("24"), SnmpValue::Number(24.0));
+        assert_eq!(SnmpValue::new("24.0384"), SnmpValue::Number(24.0384));
+    }
 }
